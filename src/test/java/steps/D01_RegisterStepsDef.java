bf02f53bc7ba741dbf3f134d92047eb0f8bb4f34
AbdAlhakimHawaf <abdalhakim.hawaf@gmail.com>
2024-03-04 23:39:43 +0200

    4th Commit adding register features

---
+++ src/test/java/steps/D01_RegisterStepsDef.java
@@ -0,0 +1,56 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class D01_RegisterStepsDef {
+    @Given(": User navigate to register page")
+    public void userNavigateToRegisterPage() {
+    }
+
+    @When(":Selecting Gender")
+    public void selectingGender() {
+    }
+
+    @And(":Filling First name")
+    public void fillingFirstName() {
+    }
+
+    @And(":Filling Last Name")
+    public void fillingLastName() {
+    }
+
+    @And(":Selecting Date of birth d-m-y")
+    public void selectingDateOfBirthDMY() {
+    }
+
+    @And(":Filling Email")
+    public void fillingEmail() {
+    }
+
+    @And(":Filling Company name")
+    public void fillingCompanyName() {
+    }
+
+    @And(":Selecting Newsletter")
+    public void selectingNewsletter() {
+    }
+
+    @And(": Filling Password")
+    public void fillingPassword() {
+    }
+
+    @And(":Filling Confirm Password the same to Password Field")
+    public void fillingConfirmPasswordTheSameToPasswordField() {
+    }
+
+    @And(":Clicking on Register Button")
+    public void clickingOnRegisterButton() {
+    }
+
+    @Then(":Success Message of Registration will appear")
+    public void successMessageOfRegistrationWillAppear() {
+    }
+}
